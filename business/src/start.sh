0a647fe1ca886c4bacc75a7d02e4c6251415feac
carlodones <dones.carlo@gmail.com>
2018-08-15 11:03:00 +0200

    upload startsh time set

--- business/src/start.sh
+++ business/src/start.sh
@@ -5,6 +5,15 @@ if [ "$INITSYSTEM" != "on" ]; then
   /usr/sbin/sshd -p 22 &
 fi
 
+# Default to UTC if no TIMEZONE env variable is set
+
+echo "Setting time zone to ${TIMEZONE=UTC}"
+# This only works on Debian-based images
+
+echo "${Europe/London}" > /etc/timezone
+dpkg-reconfigure tzdata
+
+
 echo "This is where your application would start..."
 
 python /usr/src/app/src/hello.py

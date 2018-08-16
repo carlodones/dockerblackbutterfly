dbf6fea1813cf97ddc4c73a53f4e661e362f3f9a
carlodones <dones.carlo@gmail.com>
2018-08-16 16:05:16 +0200

    set ntp timezone

--- business/src/ntp1.py
+++ business/src/ntp1.py
@@ -17,24 +17,19 @@ print(ctime(response.tx_time))
 print(ntplib.system_to_ntp_time)
 
 #print (os.system)
-#os.system('/etc/init.d/ntpd stop, /usr/sbin/ntpdate -b -s it.pool.ntp.org, /etc/init.d/ntpd start')
+
 current_time = ctime(response.tx_time)
+print (current_time)
+os.system("service ntp stop")
 os.system("sudo date -s '{0}'".format(current_time))
-#self.logger.info('Raptor rozpoczal prace: {0}'.format(current_time))
+os.system("sudo service ntp start")
 
-'''
 
-def set(self):
-        try:
-            c = ntplib.NTPClient()
-            response = c.request('europe.pool.ntp.org', version=3)
-            current_time = ctime(response.tx_time)
-            # current_time = 'Sun Apr 09 21:15:06 2017'
-            os.system("sudo date -s '{0}'".format(current_time))
-            self.logger.info('Raptor rozpoczal prace: {0}'.format(current_time))
 
-        except Exception as ex:
-            self.logger.error('OsTime: {0}'.format(ex))
-            pass 
 
-'''
+
+
+#os.system('/etc/init.d/ntpd stop, /usr/sbin/ntpdate -b -s it.pool.ntp.org, /etc/init.d/ntpd start')
+#os.system("sudo ntpdate '{0}'".format(current_time))
+#os.system("sudo timedatectl set-time '{0}'".format(current_time))
+
